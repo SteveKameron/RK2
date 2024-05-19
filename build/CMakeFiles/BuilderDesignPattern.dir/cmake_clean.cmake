f973b83002ab341a040041dff80fc1b9bac35c23
SteveKameron <scorching.k173@gmail.com>
2024-05-19 16:12:27 +0300

    githubactions

---
+++ build/CMakeFiles/BuilderDesignPattern.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "BuilderDesignPattern"
+  "BuilderDesignPattern.pdb"
+  "CMakeFiles/BuilderDesignPattern.dir/Hero.cpp.o"
+  "CMakeFiles/BuilderDesignPattern.dir/Hero.cpp.o.d"
+  "CMakeFiles/BuilderDesignPattern.dir/HeroBuilder.cpp.o"
+  "CMakeFiles/BuilderDesignPattern.dir/HeroBuilder.cpp.o.d"
+  "CMakeFiles/BuilderDesignPattern.dir/main.cpp.o"
+  "CMakeFiles/BuilderDesignPattern.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BuilderDesignPattern.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
